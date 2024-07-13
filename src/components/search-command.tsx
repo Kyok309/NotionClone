ac166799d01a750b4a626af0d44eaa62e46decd3
Kyok309 <kyok309xx@gmail.com>
2024-07-13 17:32:28 +0800

    onselect

--- src/components/search-command.tsx
+++ src/components/search-command.tsx
@@ -65,7 +65,7 @@ export const SearchCommand = () => {
               key={document._id}
               value={`${document._id}-${document.title}`}
               title={document.title}
-              onClick={() => onSelect(document._id)}
+              onSelect={() => onSelect(document._id)}
             >
               {document.icon ? (
                 <p className="mr-2 text-[18px]">
